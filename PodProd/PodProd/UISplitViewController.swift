d98069b4f12fddea37f3dba850d1f0c388a63110
Rostyslav Dydiak <rostyslav.dydiak@avenga.com>
2023-01-12 03:54:03 +0200

    add new ViewController

---
+++ PodProd/PodProd/UISplitViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UISplitViewController.swift
+//  PodProd
+//
+//  Created by Rostyslav Dydiak on 12.01.2023.
+//
+
+import Foundation
+import UIKit
+
+class UISplitViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
--- PodProd/PodProd/ViewController.swift
+++ PodProd/PodProd/ViewController.swift
@@ -6,15 +6,53 @@
 //
 
 import UIKit
+import Foundation
 import SnapKit
 
 class ViewController: UIViewController {
 
+    private lazy var buttonToUISplitViewController: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .lightGray
+        button.setTitle("UISplitViewController", for: .normal)
+        button.setTitleColor(.purple, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = .yellow
+        setupView()
     }
     
+    private func setupView() {
+        buttonToUISplitViewController.addTarget(self, action: #selector(detailsButtonTapped), for: .touchUpInside)
+        self.view.addSubview(buttonToUISplitViewController)
+        setUpConstraints()
+    }
+    
+    private func setUpConstraints() {
+        buttonToUISplitViewController.snp.makeConstraints { make in
+            make.top.equalTo(view).offset(UIConstraint.sideOffsetTop)
+            make.leading.equalTo(view).offset(UIConstraint.sideOffsetLeading)
+            make.trailing.equalTo(view).offset(-UIConstraint.sideOffsetTrailing)
+            make.bottom.equalTo(view).offset(-UIConstraint.sideOffestBottom)
+        }
+    }
+    
+    private enum UIConstraint {
+        static let sideOffsetTop = 200
+        static let sideOffsetLeading = 80
+        static let sideOffsetTrailing = 80
+        static let sideOffestBottom = 200
+    }
+    
+    @objc private func detailsButtonTapped() {
+        let splitViewController = UISplitViewController()
+        splitViewController.modalTransitionStyle = .crossDissolve
+        splitViewController.modalPresentationStyle = .fullScreen
+        present(splitViewController, animated: true)
+    }
 }
 
